5d7c7cc7e8729e6e86760796eebfa8282b50415b
t72011 <haha@schneider.com>
2018-06-05 09:01:08 -0500

    cp

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="",
+    description="",
+    author="Andrew Hah",
+    author_email="hahdawg@yahoo.com",
+	packages=setuptools.find_packages(exclude=[]),
+	install_requires=[],
+	include_package_data=True,
+    license="MIT",
+    zip_safe=False
+)
